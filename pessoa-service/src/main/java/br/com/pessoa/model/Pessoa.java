7618f2640db3eb941e62750711570a51cda2c6e6
diegosilva13 <diego.dbssilva@gmail.com>
2017-06-24 01:29:59 -0300

    Criação dos pacotes e da classe base Pessoa

---
+++ pessoa-service/src/main/java/br/com/pessoa/model/Pessoa.java
@@ -0,0 +1,14 @@
+package br.com.pessoa.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
